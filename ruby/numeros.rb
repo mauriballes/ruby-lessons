d1dd8083405ebc1ce92c20003c236c6bc2e5c67a
Mauricio Ballesteros <mballes@gmail.com>
2017-09-26 22:53:19 -0400

    Numeros

---
+++ ruby/numeros.rb
@@ -0,0 +1,32 @@
+# Numeros
+### Enteros
+
+34
+52
+1
+
+### Flotantes
+
+20.0
+25.54
+2.52
+
+# Para pasar de entero a float
+
+10.to_f
+
+# Para pasar de float a entero
+
+12.2315.to_i
+
+# Para valor absoluto
+
+-10.abs
+
+# Para saber si el valor es numero para
+
+2.even?
+
+# Para devolver el siguiente entero
+
+3.next
