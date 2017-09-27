c6e445054c971c5dcd785c0f731c8eb12ba9859f
Mauricio Ballesteros <mballes@gmail.com>
2017-09-26 22:47:31 -0400

    Variables en Ruby

---
+++ ruby/variables.rb
@@ -0,0 +1,16 @@
+# Variables en Ruby
+
+variable = "Mauricio"
+puts variable
+
+# En ruby se puede reasignar la variable
+# con otro tipo de dato
+
+variable = 22
+puts variable
+
+# La convencion de nombres de variables es
+# el snake case y minusculas
+
+nombre_completo = "Mauricio Ballesteros"
+puts nombre_completo
